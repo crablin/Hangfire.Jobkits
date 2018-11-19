2a5e61a00facd95a51ee288cede22de2a2af441f
Crab Lin <lin.crab@gmail.com>
2018-11-20 02:15:34 +0800

    add select and textarea ui

---
+++ src/Hangfire.JobKits/Dashboard/Controls/InputBox/SelectControl.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Hangfire.Dashboard;
+
+namespace Hangfire.JobKits.Dashboard.Controls.InputBox
+{
+
+    internal sealed class SelectControl : RazorPage
+    {
+        public ParameterInfo Parameter { get; }
+
+        public SelectControl(ParameterInfo parameter)
+        {
+            Parameter = parameter;
+        }
+
+        public override void Execute()
+        {
+            var param = Parameter.GetCustomAttribute<JobParamAttribute>();
+
+            var description = param?.Description ?? Parameter.Name;
+            var defaultValue = param?.DefaultValue?.ToString() ?? string.Empty;
+
+            WriteLiteral("<div class=\"input-group\">");
+            WriteLiteral($"<div class=\"input-group-addon\">{Parameter.Name}</div>");
+            WriteLiteral($"<select class=\"form-control\" name=\"{Parameter.Name}\" value=\"{defaultValue}\">");
+
+            foreach (var item in Enum.GetValues(Parameter.ParameterType))
+            {
+                var attr = item.ToString() == defaultValue ? " selected" : "";
+                WriteLiteral($"<option{attr}>{item}</option>");
+            }
+
+            WriteLiteral("</select>");
+            WriteLiteral("</div>");
+        }
+    }
+}
---
+++ src/Hangfire.JobKits/Dashboard/Controls/InputBox/TextareaControl.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using Hangfire.Dashboard;
+using Newtonsoft.Json;
+
+namespace Hangfire.JobKits.Dashboard.Controls.InputBox
+{
+
+    internal sealed class TextareaControl : RazorPage
+    {
+        public ParameterInfo Parameter { get; }
+
+        public TextareaControl(ParameterInfo parameter)
+        {
+            Parameter = parameter;
+        }
+
+        public override void Execute()
+        {
+            var param = Parameter.GetCustomAttribute<JobParamAttribute>();
+
+            var obj = Activator.CreateInstance(Parameter.ParameterType);
+
+            if (IsList(obj))
+            {
+                Type genericType = obj.GetType().GetGenericArguments()[0];
+
+                ((IList)obj).Add(Activator.CreateInstance(genericType));
+            }
+            
+            var json = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented
+            });
+
+            WriteLiteral("<div class=\"input-group\">");
+            WriteLiteral($"<div class=\"input-group-addon\">{Parameter.Name}</div>");
+            WriteLiteral($"<textarea name=\"{Parameter.Name}\" class=\"form-control json-input\">{json}</textarea>");
+            WriteLiteral("</div>");
+        }
+
+        public bool IsList(object o)
+        {
+            if (o == null) return false;
+            return o is IList &&
+                   o.GetType().IsGenericType &&
+                   o.GetType().GetGenericTypeDefinition().IsAssignableFrom(typeof(List<>));
+        }
+
+        public bool IsDictionary(object o)
+        {
+            if (o == null) return false;
+            return o is IDictionary &&
+                   o.GetType().IsGenericType &&
+                   o.GetType().GetGenericTypeDefinition().IsAssignableFrom(typeof(Dictionary<,>));
+        }
+
+    }
+}
